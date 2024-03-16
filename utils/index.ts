9f60ffc1df5ff0095392b0fcf686e3bbb55e13fb
双刃战意 <719857499@qq.com>
2024-03-16 20:15:32 +0800

    434

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,8 @@
+import { version } from './package.json'
 /**
  * 版本
  */
-export const getVer = () => 'utils - 1.0.1'
+export const getVer = () => `utils - ${version}`
 
 export const utilsHook = (name: string) => {
   if (['getVer'].includes(name)) {
